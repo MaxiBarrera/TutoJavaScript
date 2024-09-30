62947200f082dbb3b401b69129706662520d3a7d
MaxiBarrera <129546471+MaxiBarrera@users.noreply.github.com>
2024-09-30 19:16:05 -0300

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function functionRed(){
+    document.getElementById("crimson").style.color = "red";
+}
+
+function functionBlack(){
+    document.getElementById("crimson").style.color = "black";
+}
